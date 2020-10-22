8e3143b9bfef3aeb36a4e736f507e4dc7b764f5b
MapleTree856 <69950920+MapleTree856@users.noreply.github.com>
2020-10-22 14:01:12 +0800

    Update eliza.py

--- eliza.py
+++ eliza.py
@@ -3,5 +3,7 @@ import math
 ask='is something troubling you?'
 while True:
   answer=input(ask)
-if "A" appears
-  replace with "F"
+  if answer == "yes":
+    print("You can always seek a trusted adult for help, you know."
+  if answer == "no":
+    print("Well, that's awesome!")
